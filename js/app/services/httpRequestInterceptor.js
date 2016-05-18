32333d1f748d0ea00e9462ef4b74283a55c4cbcc
jsgoldb <jgoldb@yahoo.com>
2016-05-18 10:39:14 -0500

    Done.

---
+++ js/app/services/httpRequestInterceptor.js
@@ -0,0 +1,12 @@
+function httpRequestInterceptor(){
+  this.responseError = function(config){
+    alert("You have an error!");
+  }
+}
+
+angular
+  .module('app')
+  .service('httpRequestInterceptor', httpRequestInterceptor)
+  .config(function ($httpProvider) {
+    $httpProvider.interceptors.push('httpRequestInterceptor');
+  });
