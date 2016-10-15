798f888bb1f289f1a5595b69aa2ed7d176792318
sarond <saron.desta@gmail.com>
2016-10-14 23:49:56 -0400

    Done.

---
+++ js/app/services/httpRequestInterceptor.js
@@ -0,0 +1,12 @@
+function MyInterceptor() {
+  this.responseError = function() {
+    alert('Error!');
+  };
+}
+
+angular
+  .module('app')
+  .service('MyInterceptor', MyInterceptor)
+  .config(function($httpProvider){
+    $httpProvider.interceptors.push('MyInterceptor');
+  });
