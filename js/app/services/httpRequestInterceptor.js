33233e95c35c90447380280e623f856d29e320c0
michaelsimon <michael.simon@gmail.com>
2016-10-23 16:50:57 -0400

    Done.

---
+++ js/app/services/httpRequestInterceptor.js
@@ -0,0 +1,12 @@
+function MyInterceptor() {
+  this.responseError = function(config) {
+    alert('There was an error');
+  };
+}
+
+angular
+  .module('app')
+  .service('MyInterceptor', MyInterceptor)
+  .config(function($httpProvider){
+    $httpProvider.interceptors.push('MyInterceptor');
+  });
