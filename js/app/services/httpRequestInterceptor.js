1a9d8a3c964969ca76e437b7a7f05b64c99c6898
jbok4 <jbok4@aol.com>
2016-12-09 15:01:18 -0500

    complete

---
+++ js/app/services/httpRequestInterceptor.js
@@ -0,0 +1,13 @@
+function  HttpRequestInterceptor() {
+ this.responseError = function (config) {
+
+          alert(config);
+    };
+}
+ 
+angular
+    .module('app')
+    .service('HttpRequestInterceptor', HttpRequestInterceptor)
+    .config(function ($httpProvider) {
+        $httpProvider.interceptors.push('HttpRequestInterceptor');
+    });
